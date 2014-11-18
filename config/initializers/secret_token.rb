735e838264c2fa9be083cb50cf892b0ccae6d344
JoseA <jamarinhidalgo@gmail.com>
2014-11-18 13:33:17 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ListinRoR::Application.config.secret_token = 'ef933d3e738869402b60a182328b9307957e0635e3151d22ec2c974620d354f5f29f134f7daf7b3a3c20f763d411227778bab75d0fa271d261cd58707ca1749b'
