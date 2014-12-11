56f10f5153c026f0caf9463592fa41151c9d862e
JoseA <jamarinhidalgo@gmail.com>
2014-12-11 19:34:17 +0100

    new style & admin index contacts action

--- app/controllers/admin/contacts_controller.rb
+++ app/controllers/admin/contacts_controller.rb
@@ -1,9 +1,16 @@
-class Users::ContactsController < ApplicationController
+class Admin::ContactsController < ApplicationController
+  before_filter :authenticate
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |name, pass|
+      #Contact.authenticate(name, pass)
+      name == 'akita' && pass == 'akita'      
+    end
+  end
   # GET /contacts
   # GET /contacts.json
   def index
-    @user = User.find(params[:user_id])
-    @contacts = @user.contacts
+    @contacts = Contact.all
 
     respond_to do |format|
       format.html # index.html.erb
@@ -11,11 +18,10 @@ class Users::ContactsController < ApplicationController
     end
   end
 
-  # GET /contacts/1s
+  # GET /contacts/1
   # GET /contacts/1.json
   def show
     @contact = Contact.find(params[:id])
-    @user = User.find(params[:user_id])
 
     respond_to do |format|
       format.html # show.html.erb
@@ -27,7 +33,6 @@ class Users::ContactsController < ApplicationController
   # GET /contacts/new.json
   def new
     @contact = Contact.new
-    @user = User.find(params[:user_id])
 
     respond_to do |format|
       format.html # new.html.erb
@@ -38,19 +43,16 @@ class Users::ContactsController < ApplicationController
   # GET /contacts/1/edit
   def edit
     @contact = Contact.find(params[:id])
-    @user = User.find(params[:user_id])
   end
 
   # POST /contacts
   # POST /contacts.json
   def create
-    @user = User.find(params[:user_id])
-    #@user.contacts = Contact.new(params[:contact])
-    @contact = @user.contacts.create(params[:contact])
+    @contact = Contact.new(params[:Contact])
 
     respond_to do |format|
-      if @contact.save
-        format.html { redirect_to user_contact_path(@user, @contact), notice: 'Contact was successfully created.' } #manda al show de contact
+      if @Contact.save
+        format.html { redirect_to ([:admin, @contact]), notice: 'Contact was successfully created.' }
         format.json { render json: @contact, status: :created, location: @contact }
       else
         format.html { render action: "new" }
@@ -62,12 +64,11 @@ class Users::ContactsController < ApplicationController
   # PUT /contacts/1
   # PUT /contacts/1.json
   def update
-    @user = User.find(params[:user_id])
     @contact = Contact.find(params[:id])
 
     respond_to do |format|
-      if @contact.update_attributes(params[:contact])
-        format.html { redirect_to user_contact_path(@user, @contact), notice: 'Contact was successfully updated.' }
+      if @contact.update_attributes(params[:Contact])
+        format.html { redirect_to ([:admin, @contact]), notice: 'Contact was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
@@ -79,12 +80,11 @@ class Users::ContactsController < ApplicationController
   # DELETE /contacts/1
   # DELETE /contacts/1.json
   def destroy
-    @user = User.find(params[:user_id])
     @contact = Contact.find(params[:id])
     @contact.destroy
 
     respond_to do |format|
-      format.html { redirect_to user_contacts_path(@user), notice: 'Contact was successfully deleted.'  }
+      format.html { redirect_to admin_contacts_url }
       format.json { head :no_content }
     end
   end
